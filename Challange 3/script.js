b41de5cfc2325d1f8d5be563d0374d48ad2a197a
msid8148 <msid8148@uni.sydney.edu.au>
2025-09-02 20:58:06 +1000

    first commit

---
+++ Challange 3/script.js
@@ -0,0 +1,19 @@
+const arr = [1, 2, 3, 4, 5];
+
+arr.unshift(0);
+
+arr.push(6);
+
+arr.reverse();
+
+console.log(arr);
+
+
+const arr1= [1,2,3,4,5];
+const arr2= [5,6,7,8,9,10];
+
+let arr3;
+
+arr3=arr1.concat(arr2.splice(1,5));
+
+console.log(arr3);
---
+++ Challange1/script.js
@@ -0,0 +1,14 @@
+const myString= 'karigan';
+
+
+
+let myNewString= myString;
+myNewString= myString.replace(myString.charAt(0), myString.charAt(0).toUpperCase())
+
+
+
+
+
+
+
+console.log(myNewString);
